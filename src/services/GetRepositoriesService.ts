f39456b8ac8d4ff7bd110c8daee4d04fb5f6c56e
leosteil <lsteil@inf.ufsm.br>
2021-05-10 19:52:24 -0300

    Changing return to test http content

--- src/services/GetRepositoriesService.ts
+++ src/services/GetRepositoriesService.ts
@@ -11,22 +11,33 @@ interface Request {
 }
 
 class GetRepositoriesService {
-    public async execute({ language }: Request): Promise<Repository>{
+    public async execute({ language }: Request){
 
-        return await api.get(`orgs/takenet/repos?per_page=100&sort=created&direction=asc`).then((response) => {
+        let items =  await api.get(`orgs/takenet/repos?per_page=100&sort=created&direction=asc`).then((response) => {
             let data = response.data;
             return data = data.filter(repository => {
                 return repository.language == language
             }).map(repository => {
                 return {
-                    "name": repository.full_name,
-                    "description": repository.description,
-                    "avatar": repository.owner.avatar_url,
+                    "header": {
+                        "type": "application/vnd.lime.media-link+json",
+                        "value": {
+                            "title": "Plano:",
+                            "text": "Info: "                        }
+                    },
+                    // "name": repository.full_name,
+                    // "description": repository.description,
+                    // "avatar": repository.owner.avatar_url,
                 }
             }).slice(0, 5);
         }).catch((e) => {
             return e;
         });
+
+        return {
+            "itemType": "application/vnd.lime.document-select+json",
+            items
+        };
     }
 }
 
